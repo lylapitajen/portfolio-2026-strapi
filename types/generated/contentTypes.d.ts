c4423d7913b1195799c2acc6fd6d94231a2dfb28
lylapitajen <lylapitajen00@gmail.com>
2026-01-20 13:38:20 +0000

    added website field to Agency

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -494,6 +494,7 @@ export interface ApiAgencyAgency extends Struct.CollectionTypeSchema {
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    website: Schema.Attribute.String;
   };
 }
 
